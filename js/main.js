e548339e514c719b879b9f5128a42b17d6fbdd99
Aleksandr999908 <100771338+Aleksandr999908@users.noreply.github.com>
2022-10-13 15:04:35 +0500

    Delete main.js

--- js/main.js
+++
@@ -1,8 +0,0 @@
-let commentLength = function (test_string, max_length) {
-     if(test_string.length > max_length){
-           return true;
-     } else {
-        return false;
-     }
-}
-commentLength('Функция для проверки максимальной длины строки',40);
