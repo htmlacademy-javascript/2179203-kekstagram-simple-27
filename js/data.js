07a97b5fd67150dbda85157b0053b7508e155a30
github-Aleksandr999908 <aleksandr999908@mail.ru>
2022-11-01 19:21:14 +0500

    add Работа с событиями

---
+++ js/data.js
@@ -0,0 +1,41 @@
+import {getRandomArrayElement} from './util.js';
+
+const PICTURES__PHOTO = 25;
+
+const likesCount = {
+    min: 15,
+    max: 200,
+}
+
+const commentLines = {
+    min: 0,
+    max: 200,
+}
+
+const description = [
+    'Игра в футбол. #футбол',
+    'Силовые виды спорта. #силовой экстрим',
+    'Плавание на открытой воде. #плавание',
+    'Бег на длинные дистанции. #атлетика',
+    'Природа России. #горы',
+    'Город России. #Пермь',
+    'Лесные животные. #Лиса',
+    'Пешие походы. #туризм',
+    'Зимние виды. #снег',
+    'Вид спорта на льду. #коньки',
+]
+
+
+
+
+  const createPic = (index) => ({
+     id: index + 1,
+     url:`photos/${index + 1}.jpg`,
+     description:getRandomArrayElement(description),
+     likes:getRandomArrayElement(likesCount),
+     comments:getRandomArrayElement(commentLines),
+  });
+
+  const similarProject = Array.from({length: PICTURES__PHOTO},(_, index) => createPic(index));
+
+  export {similarProject};
